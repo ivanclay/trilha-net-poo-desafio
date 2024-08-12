daa9e213ab5dcfc3a025d12f99afa2806b7c194a
Ivan Clay <ivan.clay.moura@gmail.com>
2024-08-12 08:37:28 -0300

    desafio poo

--- trilha-net-poo-desafio/Program.cs
+++ trilha-net-poo-desafio/Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nk = new Nokia("9999-8888", "NOK-897", "789456QWE1234", 128);
+nk.InstalarAplicativo("LigarViaWeb");
+nk.Ligar();
+
+Iphone iph = new Iphone("9999-7777", "IPH-145", "562415TYUB15987", 512);
+iph.InstalarAplicativo("LigarViaWeb");
+iph.ReceberLigacao();
